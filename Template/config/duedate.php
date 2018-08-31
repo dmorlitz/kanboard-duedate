c3817de0df82942b33f0254d169870dec08e7408
David Morlitz <david@morlitz.com>
2018-08-31 12:45:16 -0700

    Fixed config panel text

--- Template/config/duedate.php
+++ Template/config/duedate.php
@@ -18,7 +18,10 @@
     <fieldset>
         <legend><?= t('Due date assumed for tasks without due date') ?></legend>
         <?= $this->form->text('duedate_board_default_date', $values, $errors, array('required', 'autofocus', 'tabindex="2"')) ?>
-        <br><b>NOTE:</b> Any date formated by strtotime is accepted here - a value of 0 will force undated items to the top
+        <br><b>NOTE:</b> Any date formated by strtotime is accepted here
+        <br>&nbsp&nbsp&nbspA value of 0 will force undated items to the top
+        <br>&nbsp&nbsp&nbspA value significantly in the future will force undated items to the bottom
+        <br>&nbsp&nbsp&nbspAny other date (i.e. 12/31/2019) or relative date (+75 days) will force undated items to that spot in the list
     </fieldset>
 
     <?php
