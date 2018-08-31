bbe4de30f1681fc93144d116be5dc7bf4b7202db
David Morlitz <david@morlitz.com>
2018-08-31 06:32:18 -0700

    Fix redirection URL

--- Template/project_header/views.php
+++ Template/project_header/views.php
@@ -1,7 +1,10 @@
 <?php //var_dump($this->app->request);//$_SERVER['REQUEST_URI']);?>
-<?php $redirect = $this->app->request->getServerVariable("REQUEST_SCHEME") . "://" .
-                  $this->app->request->getServerVariable("SERVER_NAME") .
-                  $this->app->request->getUri();
+<?php
+//      $redirect = $this->app->request->getServerVariable("REQUEST_SCHEME") . "://" .
+//                  $this->app->request->getServerVariable("SERVER_NAME") .
+//                  $this->app->request->getUri();
+      //Try setting redirect to path only - to auto-detect protocol
+      $redirect = $this->app->request->getUri();
       $sort = $this->app->configModel->get('duedate_board_sort_method');
       if ( $sort == "duedate_due") {
          $sorttext = "due date";
