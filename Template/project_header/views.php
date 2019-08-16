4395f92cc2465b7c3e0f8508b1070182c652fe79
David Morlitz <david@morlitz.com>
2019-08-16 10:26:33 -0400

    Added dividers on/off indicator in project header

--- Template/project_header/views.php
+++ Template/project_header/views.php
@@ -2,9 +2,14 @@
 //      $sort = $this->app->configModel->get('duedate_board_sort_method');
 
       $sort = $this->task->projectMetadataModel->get($project['id'], 'DueDate_Board_Sort_Method');
+      $dividers = $this->task->projectMetadataModel->get($project['id'], 'DueDate_Board_Dividers');
 
       if ($sort == null) {
          $sort = "duedate_board";
+      }
+
+      if ($dividers == null) {
+         $dividers = "duedate_board_dividers_off";
       }
 
       if ( $sort == "duedate_due") {
@@ -13,7 +18,13 @@
          $sorttext = "board order";
       }
 
+      if ( $dividers == "duedate_board_dividers_on") {
+         $dividertext = " / Dividers on";
+      } else {
+         $dividertext = " / Dividers off";
+      }
+
 ?>
 <li <?= $this->app->checkMenuSelection('DueDateConfigController', 'show', 'DueDate') ?>>
-    <?= $this->url->icon('sort', t('Sorted by ' . $sorttext), 'DueDateConfigController', 'show', array('plugin' => 'DueDate','project_id' => $project['id'])) ?></li>
+    <?= $this->url->icon('sort', t('Sorted by ' . $sorttext . $dividertext), 'DueDateConfigController', 'show', array('plugin' => 'DueDate','project_id' => $project['id'])) ?></li>
 </li>
